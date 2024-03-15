573037487b1d8c196a787ecf6146ccc13b2a213b
zhanglk <zhanglkx@foxmail.com>
2024-03-15 17:14:09 +0800

    添加 bind

--- InterView/bindOperator.js
+++ InterView/bindOperator.js
@@ -1,3 +1,21 @@
-Function.prototype.bind2 = function (fn, args) {
-    
+Function.prototype.bind2 = function (context) {
+  const self = this; //this代表的是调用bind的对象
+  const args = Array.prototype.slice.call(arguments, 1);
+
+  const fNOP = function () {};
+
+  const fBound = function () {
+    const bindArgs = Array.prototype.slice.call(arguments);
+    // 这里使用this instanceof fNOP会为true是因为，fBound.prototype = new fNOP();这句代码，相当于fNOP位于fBound的原型链上，
+    // 原直接将 fBound. prototype = this.prototype，我们直接修改 fBound.prototype 的时候，
+    // 也会直接修改绑定函数的 prototype。这个时候，我们可以通过一个空函数来进行中转
+    return self.apply(
+      this instanceof fNOP ? this : context,
+      args.concat(bindArgs)
+    );
+  };
+
+  fNOP.prototype = this.prototype;
+  fBound.prototype = new fNOP();
+  return fBound;
 };
